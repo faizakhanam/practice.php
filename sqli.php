6c2a95398b2c7718b2f775ffce05d081aadb7434
Haseeb Nawaz <haseebnawaz298@gmail.com>
2021-10-12 08:49:34 +0500

    SQL HELP BY HASEEB FOR FIAZA

--- sqli.php
+++ sqli.php
@@ -4,6 +4,22 @@ echo "My sql are start hare"."  "."welcom we are ready to that stage to connect 
 my sqli extenstion
 pdo(jb ik sy ziyda server py run krna ho tb pdo my code likhna)*/
 
+
+/*
+you are using it wrong it has four parameters not 3 
+$connection = mysqli_connect("localhost", "root", "", "core_ppt");
+
+  if(!mysqli_connect()){
+    echo "Database connection Error";
+    exit();
+  }
+  and locahost is one word not 2 word
+
+  $hostname,$user,$password,$db this is the right parameters
+
+
+
+*/
 $servername = "local host";
 $username = "root";
 $password = "";
